ea88040456f38ada544548053df1026e577e094f
ohyuk1220 <ohyuk1220@naver.com>
2026-07-04 14:12:56 +0900

    웹 매거진: SEO(OG태그·sitemap·robots·favicon) + 카톡 미리보기 이미지

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,8 +1,18 @@
 module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy({ assets: "assets" });
+  eleventyConfig.addPassthroughCopy({ ".nojekyll": ".nojekyll" });
+  eleventyConfig.addPassthroughCopy({ "robots.txt": "robots.txt" });
+  eleventyConfig.addGlobalData("siteUrl", "https://ohyuk1220-glitch.github.io");
+  eleventyConfig.addGlobalData("siteName", "과학 한 모금");
+  eleventyConfig.addGlobalData(
+    "siteDescription",
+    "매일의 과학을 한 모금씩. 오늘의 과학 뉴스를 쉽고 깊게 풀어드립니다."
+  );
   return {
     dir: {
       input: "content",
       includes: "../_includes",
+      data: "../_data",
       output: "_site",
     },
     markdownTemplateEngine: "njk",
